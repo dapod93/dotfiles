cc074a9aa8e457f4013a5e7dd9139cdc0c821106
dapod93 <davidliois50@gmail.com>
2026-08-31 18:24:37 +0700

    chore: update copy config in fedora install.sh

--- fedora/install.sh
+++ fedora/install.sh
@@ -6,8 +6,10 @@
 # Install flatpak
 ./flatpak/packages.sh
 
-# Copy fish config to its appropriate location
-sudo cp .config/fish/config.fish ~/.config/fish
+# Copy config to its appropriate location
+cp .config/fish/config.fish ~/.config/fish
+cp .config/starship/starship.toml ~/.config/starship
+cp -r .config/fastfetch ~/.config
 
 # Change default shell to fish
 chsh -s $(which fish)
